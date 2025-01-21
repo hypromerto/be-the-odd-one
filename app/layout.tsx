46236c26ad00f206b8ce5c738df749f441227ab5
hypromerto <aslan-mert99@hotmail.com>
2025-01-21 14:55:24 +0300

    Fix adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
+import GoogleAdsense from "../components/GoogleAdsense";
 
 const inter = Inter({ subsets: ['latin'] })
+
+const AdsenseId = process.env.NEXT_PUBLIC_ADSENSE_ID
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <head>
             <Script src="https://www.google.com/recaptcha/api.js" async defer />
-            <script async
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9435164855607747"
-                    crossOrigin="anonymous"></script>
+            <GoogleAdsense pId={AdsenseId} />
         </head>
         <body className={`${inter.className} bg-gradient-to-b from-amber-200 to-indigo-700 min-h-screen`}>
         <div className="container mx-auto px-4 py-6 sm:px-6 lg:px-8 max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl">

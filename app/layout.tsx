1faaa5553c7bd4db4fffd35e1ca60d8c2d5897f5
hypromerto <aslan-mert99@hotmail.com>
2025-01-21 14:45:45 +0300

    Implement Adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
         <head>
             <Script src="https://www.google.com/recaptcha/api.js" async defer />
+            <script async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9435164855607747"
+                    crossOrigin="anonymous"></script>
         </head>
         <body className={`${inter.className} bg-gradient-to-b from-amber-200 to-indigo-700 min-h-screen`}>
         <div className="container mx-auto px-4 py-6 sm:px-6 lg:px-8 max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl">
